9a2efd46388c1946b19864dc78a915efa09335dd
manuorla <103120494+manuorla@users.noreply.github.com>
2022-06-01 15:24:20 +0200

    Creata Classe Cliente

---
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/model/Cliente.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.iad.sigeba.model;
+
+/**
+ *
+ * @author Utente
+ */
+public class Cliente {
+
+    private String cognome;
+    private String nome;
+    private String indirizzo;
+    private Long id;
+
+    public Cliente(String cognome, String nome, String indirizzo, Long id) {
+        this.cognome = cognome;
+        this.nome = nome;
+        this.indirizzo = indirizzo;
+        this.id = id;
+    }
+
+    public Cliente() {
+    }
+
+    
+   
+
+}
