5b59cd62568694189a70ddf576c4bea782cf75db
ceda-dev-01 <l.lezzerini@coopceda.it>
2022-06-01 14:45:06 +0200

    Struttura package e progetto Java

---
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/Sigeba.java
@@ -0,0 +1,15 @@
+/*
+ */
+
+package com.iad.sigeba;
+
+/**
+ *
+ * @author use
+ */
+public class Sigeba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
