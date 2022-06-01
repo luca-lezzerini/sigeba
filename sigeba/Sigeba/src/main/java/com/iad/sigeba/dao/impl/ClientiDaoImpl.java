0a9b442a80945c3520ed09f03a8b43c443016bc4
ceda-dev-01 <l.lezzerini@coopceda.it>
2022-06-01 16:56:33 +0200

    Parziale implementazione service e dao

--- sigeba/Sigeba/src/main/java/com/iad/sigeba/dao/impl/ClientiDaoImpl.java
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/dao/impl/ClientiDaoImpl.java
@@ -2,13 +2,25 @@ package com.iad.sigeba.dao.impl;
 
 import com.iad.sigeba.dao.ClientiDao;
 import com.iad.sigeba.model.Cliente;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ClientiDaoImpl  implements ClientiDao {
+public class ClientiDaoImpl implements ClientiDao {
+
+    // corrisponde al nostro database
+    private final List<Cliente> clienti = new ArrayList<>();
 
     @Override
-    public List<Cliente> cercaCliente(String cliente) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<Cliente> cercaCliente(String testoDaCercare) {
+        List<Cliente> risultato = new ArrayList<>();
+        for (Cliente cli : clienti) {
+            if (cli.getNome().contains(testoDaCercare)
+                    || cli.getCognome().contains(testoDaCercare)) {
+                risultato.add(cli);
+            }
+        }
+        
+        return risultato;
     }
 
     @Override
@@ -20,11 +32,10 @@ public class ClientiDaoImpl  implements ClientiDao {
     public Cliente leggiCliente(long idCli) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
     public void rimuoviCliente(long idCli) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
 
 }
--- sigeba/Sigeba/src/main/java/com/iad/sigeba/service/impl/ClientiServiceImpl.java
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/service/impl/ClientiServiceImpl.java
@@ -1,34 +1,40 @@
 package com.iad.sigeba.service.impl;
 
+import com.iad.sigeba.dao.ClientiDao;
+import com.iad.sigeba.factories.SigebaDaoFactory;
 import java.util.List;
 
 import com.iad.sigeba.model.Cliente;
 import com.iad.sigeba.service.ClientiService;
 
-public class ClientiServiceImpl implements ClientiService{
+public class ClientiServiceImpl implements ClientiService {
 
-	@Override
-	public List<Cliente> cercaCliente(String cliente) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private final ClientiDao clientiDao = SigebaDaoFactory.getClientiDao();
 
-	@Override
-	public Cliente salvaCliente(Cliente cli) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Cliente> cercaCliente(String cliente) {
+        if (cliente == null) {
+            throw new RuntimeException();
+        }
+        return clientiDao.cercaCliente(cliente);
+    }
 
-	@Override
-	public Cliente leggiCliente(long idCli) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Cliente salvaCliente(Cliente cli) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void rimuoviCliente(long idCli) {
-		// TODO Auto-generated method stub
+    @Override
+    public Cliente leggiCliente(long idCli) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	}
+    @Override
+    public void rimuoviCliente(long idCli) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
