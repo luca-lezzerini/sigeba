67987bc261d25ea2c0a993f849babdbb66d6d270
manuorla <103120494+manuorla@users.noreply.github.com>
2022-06-01 15:42:13 +0200

    aggiunti accessori

--- sigeba/Sigeba/src/main/java/com/iad/sigeba/model/Cliente.java
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/model/Cliente.java
@@ -22,6 +22,38 @@ public class Cliente {
         this.id = id;
     }
 
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Cliente() {
     }
 
