5236340dda951dbe3f6b8c3311700dc9089cde80
enriper <enrico.perna@iad2.it>
2022-06-01 17:22:04 +0200

    ClientiServiceImpl and ClientiService fixed

--- sigeba/Sigeba/src/main/java/com/iad/sigeba/service/impl/ClientiServiceImpl.java
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/service/impl/ClientiServiceImpl.java
@@ -1,40 +1,42 @@
 package com.iad.sigeba.service.impl;
 
-import com.iad.sigeba.dao.ClientiDao;
-import com.iad.sigeba.factories.SigebaDaoFactory;
 import java.util.List;
 
+import com.iad.sigeba.dao.ClientiDao;
+import com.iad.sigeba.factories.SigebaDaoFactory;
 import com.iad.sigeba.model.Cliente;
 import com.iad.sigeba.service.ClientiService;
 
 public class ClientiServiceImpl implements ClientiService {
 
-    private final ClientiDao clientiDao = SigebaDaoFactory.getClientiDao();
+	private final ClientiDao clientiDao = SigebaDaoFactory.getClientiDao();
 
-    @Override
-    public List<Cliente> cercaCliente(String cliente) {
-        if (cliente == null) {
-            throw new RuntimeException();
-        }
-        return clientiDao.cercaCliente(cliente);
-    }
+	@Override
+	public List<Cliente> cercaCliente(String cliente) {
+		if (cliente == null) {
+			throw new RuntimeException();
+		}
+		return clientiDao.cercaCliente(cliente);
+	}
 
-    @Override
-    public Cliente salvaCliente(Cliente cli) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public Cliente salvaCliente(Cliente cli) {
+		if (cli == null) {
+			throw new RuntimeException();
+		}
+		return clientiDao.salvaCliente(cli);
+	}
 
-    @Override
-    public Cliente leggiCliente(long idCli) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public Cliente leggiCliente(Long idCli) {
+		if (idCli <= 0) {
+			throw new RuntimeException();
+		}
+		return clientiDao.leggiCliente(idCli);
+	}
 
-    @Override
-    public void rimuoviCliente(long idCli) {
-        // TODO Auto-generated method stub
-
-    }
+	@Override
+	public void rimuoviCliente(Long idCli) {
+	}
 
 }
