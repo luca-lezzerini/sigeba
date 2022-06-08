b69ffe0450fd05e9b26cea800439ae241640c466
FranCian1982 <96352321+FranCian1982@users.noreply.github.com>
2022-06-08 12:31:13 +0200

    Update ContiCorrentiDaoImpl.java

--- sigeba/Sigeba/src/main/java/com/iad/sigeba/dao/impl/ContiCorrentiDaoImpl.java
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/dao/impl/ContiCorrentiDaoImpl.java
@@ -1,6 +1,7 @@
 package com.iad.sigeba.dao.impl;
 
 import com.iad.sigeba.dao.ContiCorrentiDao;
+import com.iad.sigeba.model.Cliente;
 import com.iad.sigeba.model.ContoCorrente;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,26 @@ public class ContiCorrentiDaoImpl implements ContiCorrentiDao{
 
 private final List<ContoCorrente> conticorrenti = new ArrayList<>();
     private Long idCorrente = 1L;
+
+    @Override
+    public List<ContoCorrente> cercaCCPerCliente(Cliente cli, String cc) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ContoCorrente salvaCC(ContoCorrente cc) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ContoCorrente leggiCC(Long idContoCorrente) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void rimuoviCC(Long idContoCorrente) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 
 }
