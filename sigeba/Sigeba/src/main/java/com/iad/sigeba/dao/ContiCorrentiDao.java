6fd7aee708e860f33b84804ac106dcfa1d61156b
manuorla <103120494+manuorla@users.noreply.github.com>
2022-06-08 12:04:24 +0200

    Create ContiCorrentiDao.java

---
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/dao/ContiCorrentiDao.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.iad.sigeba.dao;
+
+import com.iad.sigeba.model.Cliente;
+import com.iad.sigeba.model.ContoCorrente;
+import java.util.List;
+
+/**
+ *
+ * @author Utente
+ */
+public interface ContiCorrentiDao {
+    List<ContoCorrente> cercaCCPerCliente(Cliente cli,String cc);
+
+    ContoCorrente salvaCC(ContoCorrente cc);
+
+    ContoCorrente leggiCC(Long idContoCorrente);
+
+    void rimuoviCC(Long idContoCorrente);
+    
+    
+}
