a60ecd20b432ea098a206575981a698ff4701782
enriper <enrico.perna@iad2.it>
2022-06-08 10:25:50 +0200

    Create ContiCorrentiService.java
    ContiCorrentiService interface created

---
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/service/ContiCorrentiService.java
@@ -0,0 +1,18 @@
+package com.iad.sigeba.service;
+
+import java.util.List;
+
+import com.iad.sigeba.model.Cliente;
+import com.iad.sigeba.model.ContoCorrente;
+
+public interface ContiCorrentiService {
+
+	List <ContoCorrente> cercaCCPerCliente (Cliente cli, String stringCC);
+
+	ContoCorrente salvaCC (ContoCorrente cc);
+
+	ContoCorrente leggiCC (Long id);
+
+	void rimuoviCC (ContoCorrente cc);
+
+}
