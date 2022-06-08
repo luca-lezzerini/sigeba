e889e732900329e7cb52644d278b583fd3acc2b0
FranCian1982 <96352321+FranCian1982@users.noreply.github.com>
2022-06-08 12:27:09 +0200

    Update ContiCorrentiDaoImpl.java

--- sigeba/Sigeba/src/main/java/com/iad/sigeba/dao/impl/ContiCorrentiDaoImpl.java
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/dao/impl/ContiCorrentiDaoImpl.java
@@ -1,10 +1,11 @@
 package com.iad.sigeba.dao.impl;
 
+import com.iad.sigeba.dao.ContiCorrentiDao;
 import com.iad.sigeba.model.ContoCorrente;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ContiCorrentiDaoImpl implements CCDao{
+public class ContiCorrentiDaoImpl implements ContiCorrentiDao{
 
 private final List<ContoCorrente> conticorrenti = new ArrayList<>();
     private Long idCorrente = 1L;
