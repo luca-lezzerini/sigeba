46debd59e94e7e7a83caf1b1e1f26fb761cf8b26
FranCian1982 <96352321+FranCian1982@users.noreply.github.com>
2022-06-08 12:17:00 +0200

    create ContiCorrentiDaoImpl

--- sigeba/Sigeba/src/main/java/com/iad/sigeba/proxies/CCServiceProxy.java
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/proxies/CCServiceProxy.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.iad.sigeba.proxies;
 
-/**
- *
- * @author ricca
- */
-public class CCServiceProxy {
+import com.iad.sigeba.model.Cliente;
+import com.iad.sigeba.model.ContoCorrente;
+import com.iad.sigeba.service.ContiCorrentiService;
+import java.util.List;
+
+public class CCServiceProxy implements ContiCorrentiService{
+  
     
+    
+    private final ContiCorrentiService contiCorrentiService = new ContiCorrentServiceImpl();
+
+    @Override
+    public List<ContoCorrente> cercaCCPerCliente(Cliente cli, String stringCC) {
+        return contiCorrentiService.cercaConto(conto);
+    }
+
+    @Override
+    public ContoCorrente salvaCC(ContoCorrente cc) {
+        return contiCorrentiService.salvaCC(cc);
+    }
+
+    @Override
+    public ContoCorrente leggiCC(Long id) {
+       return contiCorrentiService.leggiCC(id);
+    }
+
+    @Override
+    public void rimuoviCC(ContoCorrente cc) {
+        contiCorrentiService.rimuoviCC(cc);
+    }
+
+
+
+
 }
