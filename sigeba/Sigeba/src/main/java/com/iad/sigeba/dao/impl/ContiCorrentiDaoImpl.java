a362cf953ad7cce2c32f3a3e7bf6a2cb941b03c4
Feder91 <federicochilelli@gmail.com>
2022-06-08 13:13:51 +0200

    Update ContiCorrentiDaoImpl.java

--- sigeba/Sigeba/src/main/java/com/iad/sigeba/dao/impl/ContiCorrentiDaoImpl.java
+++ sigeba/Sigeba/src/main/java/com/iad/sigeba/dao/impl/ContiCorrentiDaoImpl.java
@@ -8,27 +8,50 @@ import java.util.List;
 
 public class ContiCorrentiDaoImpl implements ContiCorrentiDao{
 
-private final List<ContoCorrente> conticorrenti = new ArrayList<>();
+private final List<ContoCorrente> contiCorrenti = new ArrayList<>();
     private Long idCorrente = 1L;
 
     @Override
     public List<ContoCorrente> cercaCCPerCliente(Cliente cli, String cc) {
-        
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
+    
     @Override
-    public ContoCorrente salvaCC(ContoCorrente cc) {
-        
+    public ContoCorrente salvaCC(ContoCorrente contoCorrenteDaSalvare) {
+        ContoCorrente risultato = null;
+        if (contoCorrenteDaSalvare.getId() == null) {
+            contoCorrenteDaSalvare.setId(idCorrente++);
+            contiCorrenti.add(contoCorrenteDaSalvare);
+            risultato = contoCorrenteDaSalvare;
+        } else {
+            boolean trovato = false;
+            for (ContoCorrente cc : contiCorrenti) {
+                if (cc.getId().equals(contoCorrenteDaSalvare.getId())) {
+                    cc.setNumero(contoCorrenteDaSalvare.getNumero());
+                    risultato = cc;
+                    trovato = true;
+                    break;
+                }
+            }
+            if (!trovato) {
+                throw new RuntimeException();
+            }
+        }
+        return risultato;
     }
 
     @Override
     public ContoCorrente leggiCC(Long idContoCorrente) {
-        
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public void rimuoviCC(Long idContoCorrente) {
-        
+        for (ContoCorrente cc : contiCorrenti) {
+            if (cc.getId().equals(idContoCorrente)) {
+                contiCorrenti.remove(cc);
+            }
+        }
     }
 
 
